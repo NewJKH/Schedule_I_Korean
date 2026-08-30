5c8f0f999d5feeba3c085077476e57499f5be9d9
JKH <newsun0805@gmail.com>
2026-08-30 22:47:23 +0900

    feat: 화면에 나온 미번역 문구를 파일로 수집 (#23)
    정적 스캔으로는 화면 문구와 에셋 이름을 구분할 수 없어, 후킹이 보는 문구 중
    번역 실패한 것만 UserData에 모은다. 이 목록이 번역 작업 목록이 된다.

--- src/KoreanTextFixer/Plugin.Melon.cs
+++ src/KoreanTextFixer/Plugin.Melon.cs
@@ -4,7 +4,7 @@ using System.IO;
 using MelonLoader;
 using MelonLoader.Utils;
 
-[assembly: MelonInfo(typeof(KoreanTextFixer.MelonPlugin), "Korean Text Fixer", "1.8.0", "Schedule I 한글패치")]
+[assembly: MelonInfo(typeof(KoreanTextFixer.MelonPlugin), "Korean Text Fixer", "1.9.0", "Schedule I 한글패치")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace KoreanTextFixer
@@ -25,9 +25,16 @@ namespace KoreanTextFixer
                 var cfg = MelonPreferences.CreateCategory("KoreanTextFixer", "한글패치");
                 var useHook = cfg.CreateEntry("TmpHook", true, "TMP 후킹", "끄면 폴링만으로 번역합니다");
                 var usePolling = cfg.CreateEntry("Polling", true, "폴링 보정", "끄면 후킹만으로 번역합니다");
+                var logMissing = cfg.CreateEntry("LogMissing", true, "미번역 수집",
+                    "화면에 나왔지만 번역이 없는 문구를 UserData\\KoreanTextFixer_missing.txt 에 모읍니다");
 
                 // MelonMod판 XUnity.AutoTranslator는 번역 파일을 게임폴더\AutoTranslator 아래에 둔다
                 Translations.Load(Path.Combine(MelonEnvironment.GameRootDirectory, "AutoTranslator", "Translation", "ko", "Text"));
+
+                if (logMissing.Value)
+                {
+                    MissingLog.Enable(Path.Combine(MelonEnvironment.UserDataDirectory, "KoreanTextFixer_missing.txt"));
+                }
 
                 // XUnity가 MelonLoader에서 TMP를 후킹하지 못하므로 직접 건다 (자세한 이유는 TmpHook.Melon.cs)
                 if (useHook.Value) TmpHook.Install(HarmonyInstance);
@@ -35,7 +42,7 @@ namespace KoreanTextFixer
                 if (!usePolling.Value) KLog.Info("설정에 따라 폴링을 끕니다");
 
                 _fixer = new Fixer(usePolling.Value);
-                KLog.Info("KoreanTextFixer 1.8.0 (MelonLoader) loaded. entries=" + Translations.Dict.Count + " rules=" + Rules.Count);
+                KLog.Info("KoreanTextFixer 1.9.0 (MelonLoader) loaded. entries=" + Translations.Dict.Count + " rules=" + Rules.Count);
             }
             catch (Exception e)
             {
